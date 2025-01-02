b352fe511261b445c1c2661ed73d9bee71b46e38
jenmwa <jenmwa@gmail.com>
2025-01-02 18:16:39 +0100

    add Player class & remove HighscoreInterface

--- src/helpers/renderHighScoreHTML.ts
+++ src/helpers/renderHighScoreHTML.ts
@@ -1,9 +1,19 @@
+
 import { highscoreContainer } from '../main';
-import { highscoreList } from '../models/IHighscore';
+import { Player } from '../models/Player';
+
+export const highscoreList: Player[] = [
+  new Player('Alice', 10, '00:05:23', new Date()),
+  new Player('Bob', 15, '00:06:10', new Date())
+];
 
 export function renderHighscoreSection() {
   if (highscoreContainer) {
     highscoreContainer.innerHTML = 'lets fill this list';
-    console.log('highscorelist is:', highscoreList);
-  }
+   console.log('highscorelist is:', highscoreList);
+  // }
+if (highscoreList) {
+  console.log('highscoreList is:', highscoreList);
+}
+}
 }
---
+++ src/models/Player.ts
@@ -0,0 +1,8 @@
+export class Player  {
+  constructor (
+  public playerName: string,
+  public quizPoints: number,
+  public time: string,
+  public date: Date,
+  ) {}
+}
